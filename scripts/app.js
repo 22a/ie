be3027e7e8685ee5179b7361e662868448f75678
22a <meehanp2@tcd.ie>
2015-10-11 01:47:17 +0100

    :ocean: scrapped it all, started again, CLEAN

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+angular.module('meehan', [
+  'ngRoute'
+])
+.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'partials/home.html',
+    controller: 'HomeController'
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
+  $locationProvider.html5Mode(true);
+}]);
