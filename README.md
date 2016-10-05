90b1909c3673c059c6f19da1a1c3d8e60bb60068
22a <meehanp2@tcd.ie>
2016-10-05 13:43:24 +0100

    fall cleaning :fallen_leaf:

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-# [Meehan.co](http://www.meehan.co) (via 22a.github.io)
-
-A site like site that does site like things and behaves in a manner which society has come to expect from a site.
-
-## Prerequisites
-* Vim
+# [meehan.co](https://meehan.co) staging
--- scripts/home-ctrl.js
+++
@@ -1,9 +0,0 @@
-angular.module('meehan')
-.controller('HomeController', ['$scope', '$filter', function($scope, $filter) {
-  var today = new Date();
-  $scope.year = $filter("date")(today,'yyyy') - 1995;
-  if((parseInt($filter("date")(today,'M')) < 4) || (($filter("date")(today,'M') === "4" )&&(parseInt($filter("date")(today,'d')) < 13 )))
-  {
-      $scope.year = $scope.year - 1;
-  }
-}]);
