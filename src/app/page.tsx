39f11d56f394dcccb63f56181d4cf6df59998ce5
Peter Meehan <peter.meehan@intercom.io>
2024-12-21 02:08:14 +0000

    small meta cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="relative w-full h-screen">
       <OzmaPortal />
       <div className="absolute inset-0 backdrop-blur-sm flex items-center justify-center">
-        <main className="flex items-center justify-center m-auto max-w-[70%] px-20 py-10 backdrop-blur-sm bg-foreground bg-opacity-75 text-background  box-border shadow-sm rounded-lg">
+        <main className="flex items-center justify-center m-auto max-w-[70%] px-24 py-8 backdrop-blur-sm bg-foreground bg-opacity-75 text-background  box-border shadow-sm rounded-lg">
           <header className="text-center">
             <h1 className="text-2xl font-bold">Peter Meehan</h1>
             <p className="text-sm italic ml-1">internet website</p>
